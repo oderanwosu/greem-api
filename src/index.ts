8d2c2602de890c01573ff18b388addd774cfc022
Odera Nwosu <oderanwosu.j@gmail.com>
2023-12-14 14:16:57 -0800

    Added messaging logging

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,27 @@
 import express, { json } from "express";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
+import * as fs from "fs";
 
 dotenv.config();
 var app = express();
 //middleware
 app.use(bodyParser.json({ limit: "30mb" }));
 
+app.get("/", (req, res) => {
+  res.send({ message: "hello" });
+});
+
 export const server = app.listen(process.env.AUTH_SERVER_PORT, async () => {
   console.log(
-    `🔐 Authentication server is now running at http://localhost:${process.env.AUTH_SERVER_PORT}/`
+    `AUTH SERVER server is now running at http://localhost:${process.env.AUTH_SERVER_PORT}/`
   );
 });
 
+//log information 
 server.on("request", (req, res) => {
-  let ipAdress = req.headers["x-forwarded-for"];
-
-  console.log(
-    `🔐 AUTH SERVER REQUEST -`,
-    req.method,
-    req.url,
-    "from",
-    ipAdress
-  );
+  let address = (req.socket.address() as { address: String }).address;
+  let log = `🔐 AUTH SERVER REQUEST - ${req.method} ${req.url} from ${address}`;
+  console.log(log);
+//   fs.writeFileSync("./logs/auth_request.txt", log);
 });
